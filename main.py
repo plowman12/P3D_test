53801df7c770ef9c76fec5e6cf549a7e594d03e5
plowman12 <benjamindeanhenderson@gmail.com>
2025-04-02 17:02:25 -0600

    added map data file and main program

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from map import maze
+
+window = pygame.display.set_mode((1000, 600))
+W, H = window.get_size()
+
+class camera:
+     
+    def __init__(self, Xpos, Ypos):
+        self.Pos = {"X": Xpos, "Y": Ypos}
+        self.Dir = {"X": -1, "Y": 0}
+        self.Plane = {"X": 0, "Y": 0.16}
+    
+    def draw(self):
+        for x in range(W):
+            camX = None
+            break
